e49d21fbb4025ed13484e946294cd1086c5aacc9
Robin Gottfried <robin.gottfried@fragaria.cz>
2023-06-13 18:31:27 +0200

    typoo

--- install-karmen-on-pws.sh
+++ install-karmen-on-pws.sh
@@ -2,14 +2,22 @@
 # Adds karmen on PWS printer (Klipper firmware, systemd based on debian)
 
 set -e  # exit on error
+set -u  # error on undefined
+
+DEVICE_KEY="${1:-}"
+
+if [ "$DEVICE_KEY" = "" ]; then
+    echo "Missing device key parameter."
+    exit 255
+fi
 
 
 # NodeJS
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E bash - && sudo apt install nodejs -y
+curl -fsSL https://deb.nodesource.com/setup_19.x | sudo -E /bin/bash - && sudo apt install nodejs -y
 
 # Websocket Proxy
-curl -s https://raw.githubusercontent.com/fragaria/karmen-gists/v0.0.5/ws-install.sh | sudo bash -s KEY
+curl -s https://raw.githubusercontent.com/fragaria/karmen-gists/v0.0.5/ws-install.sh | sudo -E bash -xs "$DEVICE_KEY"
 
 # karmen-pws-connector
-curl -s https://raw.githubusercontent.com/fragaria/karmen-pws-connector/v0.0.1/install.sh | exec bash -xs
+curl -s https://raw.githubusercontent.com/fragaria/karmen-pws-connector/v0.0.1/install.sh | sudo -E bash -xs
 
